afce7eb0defc275788b2cc8affe266475e6f2565
JiangGua <jonbgua@gmail.com>
2021-10-11 19:37:44 +0800

    LaTeX公式 + 自动加空格

---
+++ docs/js/config.js
@@ -0,0 +1,19 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
+  }
+};
+document$.subscribe(() => { 
+  MathJax.typesetPromise()
+})
+
+//添加空格
+pangu.spacingPageBody();
+
